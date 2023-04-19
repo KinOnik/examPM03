bd6e51dfffc504dd50749014cc0f83d616c882c9
stu-pksp119 <stu-pksp119@AMDVLA3511GW09>
2023-04-19 08:45:41 +0300

    Create class Trip

---
+++ src/exam/Trip.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exam
+{
+    class Trip
+    {
+        string duration;
+        double price;
+        int sizeGroup;
+    }
+}
