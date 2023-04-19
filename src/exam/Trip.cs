3bad7b580dcbcb37dca0e177211b8466761e3e16
stu-pksp119 <stu-pksp119@AMDVLA3511GW09>
2023-04-19 08:56:17 +0300

    edit class Trip

--- src/exam/Trip.cs
+++ src/exam/Trip.cs
@@ -6,8 +6,41 @@ namespace exam
 {
     class Trip
     {
-        string duration;
+        private string duration1;
+
+        private string Getduration()
+        {
+            return duration1;
+        }
+
+        private void Setduration(string value)
+        {
+            duration1 = value;
+        }
+
         double price;
+
+        private double Price()
+        {
+            return price;
+        }
+
+        private void Price(double value)
+        {
+            price = value;
+        }
+
         int sizeGroup;
+
+        private int SizeGroup()
+        {
+            return sizeGroup;
+        }
+
+        private void SizeGroup(int value)
+        {
+            sizeGroup = value;
+        }
+
     }
 }
