efc0ece95792a3b9e7e36099c27195e3f0d70295
stu-pksp119 <stu-pksp119@AMDVLA3511GW09>
2023-04-19 09:12:30 +0300

    add sorting TouristicOperator

--- src/exam/TouristicOperator.cs
+++ src/exam/TouristicOperator.cs
@@ -26,7 +26,33 @@ namespace exam
                 Console.Write("\n\t\tУкажите размер группы: ");
                 groupInfo[i].SetSizeGroup(Convert.ToInt32(Console.ReadLine()));
             }
+        }
 
+        public void SortGroup()
+        {
+            Console.WriteLine("\tСортировка запущена.");
+            for (int i = 0; i < groupInfo.Length; i++)
+            {
+                for (int j = 0; j < groupInfo.Length - 1 - i; j++)
+                {
+                    if(groupInfo[j].GetPrice() == groupInfo[j + 1].GetPrice())
+                    {
+                        if (groupInfo[j].GetSizeGroup() > groupInfo[j + 1].GetSizeGroup())
+                        {
+                            Trip temp = groupInfo[j + 1];
+                            groupInfo[j + 1] = groupInfo[j];
+                            groupInfo[j] = temp;
+                        }
+                    }
+                    if (groupInfo[j].GetPrice() > groupInfo[j + 1].GetPrice())
+                    {
+                        Trip temp = groupInfo[j + 1];
+                        groupInfo[j + 1] = groupInfo[j];
+                        groupInfo[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("\tСортировка окончена.");
         }
     }
 }
